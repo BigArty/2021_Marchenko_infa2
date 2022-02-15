7726083c2a6324e832e002e2b24a5f12b375df44
BigArty <artyemiy0@gmail.com>
2022-02-15 16:08:43 +0300

    New files of lab2

---
+++ lab2/main.py
@@ -0,0 +1,6 @@
+import modules.module as mod
+
+a = mod.bar(10)
+print(a)
+temp = mod.Temp()
+temp.foo()
---
+++ lab2/modules/module.py
@@ -0,0 +1,12 @@
+class Temp:
+    @staticmethod
+    def foo():
+        print('Delete me!')
+
+
+def bar(x):
+    return -x
+
+
+if __name__ == '__main__':
+    print('Hello module!')
